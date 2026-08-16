2d96dce05065bf6ecf2d173db988e995c588121a
adigrzelak284 <adigrzelak284@gmail.com>
2026-08-16 19:00:04 +0200

    Prepare store launch configuration

---
+++ public/launch-config.js
@@ -0,0 +1,10 @@
+window.GYMPROGRES_LAUNCH = {
+  googlePlay: {
+    live: false,
+    url: 'https://play.google.com/store/apps/details?id=pl.gymprogres.app'
+  },
+  appStore: {
+    live: false,
+    url: ''
+  }
+};
