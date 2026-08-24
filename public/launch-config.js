4044dd9e93327f12f73135ef020d253d50d03200
adigrzelak284 <adigrzelak284@gmail.com>
2026-08-24 23:36:27 +0200

    Enable App Store link after iOS release

--- public/launch-config.js
+++ public/launch-config.js
@@ -4,7 +4,7 @@ window.GYMPROGRES_LAUNCH = {
     url: 'https://play.google.com/store/apps/details?id=pl.gymprogres.app'
   },
   appStore: {
-    live: false,
-    url: ''
+    live: true,
+    url: 'https://apps.apple.com/pl/app/gymprogres/id6797895050?l=pl'
   }
 };
